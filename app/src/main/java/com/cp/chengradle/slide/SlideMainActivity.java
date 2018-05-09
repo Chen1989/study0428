263f41f765816cd3c30fb9d85a572edb15bcd3ff
Chen1989 <554131215@qq.com>
2018-05-09 23:16:43 +0800

    **侧边栏测试**

--- app/src/main/java/com/cp/chengradle/slide/SlideMainActivity.java
+++ app/src/main/java/com/cp/chengradle/slide/SlideMainActivity.java
@@ -3,6 +3,7 @@ package com.cp.chengradle.slide;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.Button;
 
 import com.cp.chengradle.R;
 
@@ -12,14 +13,35 @@ import com.cp.chengradle.R;
 
 public class SlideMainActivity extends Activity {
 
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.slide_view);
+        btn = findViewById(R.id.btn_chen);
+
+        SlideController controller = new SlideController(new SlideController.DragCallBack() {
+            @Override
+            public void callBackDragBagin() {
+
+            }
+
+            @Override
+            public void callBackDrag(float detaX, float detaY, float allDetaX, float allDetaY) {
+                btn.scrollBy((int)detaX, (int)detaY);
+            }
+
+            @Override
+            public void callBackDragEnd() {
+
+            }
+        });
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
         return super.onTouchEvent(event);
     }
 }

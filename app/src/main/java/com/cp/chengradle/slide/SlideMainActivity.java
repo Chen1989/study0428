048e60740ccd0105a90fcc9cb036d671d14e59eb
peng.chen <peng.chen@joyreach.com>
2018-05-18 22:11:09 +0800

    **侧滑待优化,测试而已**

--- app/src/main/java/com/cp/chengradle/slide/SlideMainActivity.java
+++ app/src/main/java/com/cp/chengradle/slide/SlideMainActivity.java
@@ -1,6 +1,8 @@
 package com.cp.chengradle.slide;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,14 @@ import android.view.View;
 import android.widget.Button;
 
 import com.cp.chengradle.R;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by PengChen on 2018/5/9.
@@ -66,6 +76,77 @@ public class SlideMainActivity extends Activity {
 //        }).start();
         Log.i("ChenSdk", "点击了按钮 3 = " + Build.PRODUCT);
 
+        try {
+            String[] asserts = getAssets().list("");
+            Log.i("ChenSdk", "asserts.length = " + getProcessName());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    private String getProcessName() {
+        int pid = android.os.Process.myPid();
+        String processName = "Kingplug";
+        ActivityManager manager = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo process: manager.getRunningAppProcesses()) {
+            if(process.pid == pid)
+            {
+                processName = process.processName;
+
+            }
+        }
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(processName.getBytes());
+            return md5C(md.digest());
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return processName;
+    }
+
+    private final String md5C(byte[] var0) {
+        StringBuilder var1 = new StringBuilder();
+        byte[] var2 = var0;
+        int var3 = var0.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            byte var5 = var2[var4];
+            var1.append(Integer.toString((var5 & 255) + 256, 16).substring(1));
+        }
+
+        return var1.toString();
+    }
+
+    private static String getProcessName(int pid) {
+        File file = new File("/proc/" + pid + "/cmdline");
+        String processName = System.getProperty("vrMachineName", "Kingplug");
+        if (file.canRead())
+        {
+            try
+            {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+                processName = reader.readLine();
+                reader.close();
+                if (processName != null && !"".equals(processName.trim()))
+                    return processName.trim();
+
+                return processName;
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return processName;
     }
 
     @Override

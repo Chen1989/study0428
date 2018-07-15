c6715f9ad10d9baa5b39ab05cfa4bd651e512323
Chen1989 <554131215@qq.com>
2018-07-15 23:10:02 +0800

    **json解析** 1.Mod 对象池

--- security/src/main/java/com/chen/security/pool/Test.java
+++ security/src/main/java/com/chen/security/pool/Test.java
@@ -7,28 +7,29 @@ package com.chen.security.pool;
 public class Test {
     public static void main(String[] arg) {
         //创建对象池工厂
-        ObjectPoolFactory poolFactory = ObjectPoolFactory.getInstance();
-        //定义所创建对象池的属性
-        ParameterObject paraObj = new ParameterObject(2,1);
-        //利用对象池工厂,创建一个存放StringBuffer类型对象的对象池
-        ObjectPool pool = poolFactory.createPool(paraObj,StringBuffer.class);
-        //从池中取出一个StringBuffer对象
-        StringBuffer buffer = (StringBuffer)pool.getObject();
-        //使用从池中取出的StringBuffer对象
-        buffer.append("hello");
-        System.out.println(buffer.toString());
-        buffer = (StringBuffer)pool.getObject();
-        //使用从池中取出的StringBuffer对象
-        buffer.append("hello1");
-        System.out.println(buffer.toString());
-        buffer = (StringBuffer)pool.getObject();
-        //使用从池中取出的StringBuffer对象
-        buffer.append("hello2");
-        System.out.println(buffer.toString());
+//        ObjectPoolFactory poolFactory = ObjectPoolFactory.getInstance();
+//        //定义所创建对象池的属性
+//        ParameterObject paraObj = new ParameterObject(2,1);
+//        //利用对象池工厂,创建一个存放StringBuffer类型对象的对象池
+//        ObjectPool pool = poolFactory.createPool(paraObj,StringBuffer.class);
+//        //从池中取出一个StringBuffer对象
+//        StringBuffer buffer = (StringBuffer)pool.getObject();
+//        //使用从池中取出的StringBuffer对象
+//        buffer.append("hello");
+//        System.out.println(buffer.toString());
+//        buffer = (StringBuffer)pool.getObject();
+//        //使用从池中取出的StringBuffer对象
+//        buffer.append("hello1");
+//        System.out.println(buffer.toString());
+//        buffer = (StringBuffer)pool.getObject();
+//        //使用从池中取出的StringBuffer对象
+//        buffer.append("hello2");
+//        System.out.println(buffer.toString());
 
-        buffer = (StringBuffer)pool.getObject();
-        //使用从池中取出的StringBuffer对象
-        buffer.append("hello3");
-        System.out.println(buffer.toString());
+        ObjectPoolChen objPool = new ObjectPoolChen();
+        objPool.createPool();
+        Object obj = objPool.getObject();
+        objPool.returnObject(obj);
+        objPool.closeObjectPool();
     }
 }

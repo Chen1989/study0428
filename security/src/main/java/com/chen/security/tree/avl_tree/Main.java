eea5292e8da2ac58a809ad179177e61f5daaf55d
Chen1989 <554131215@qq.com>
2019-01-01 22:57:40 +0800

    **日历** 1.Mod ACM

--- security/src/main/java/com/chen/security/tree/avl_tree/Main.java
+++ security/src/main/java/com/chen/security/tree/avl_tree/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        question1003();
+        question1005();
     }
 
 //    private static void question1002() {
@@ -23,46 +23,66 @@ public class Main {
 //        }
 //    }
 
-    private static void question1003() {
+//    private static void question1003() {
+//        Scanner cin = new Scanner(System.in);
+//        while (cin.hasNext()) {
+//            int T = cin.nextInt();
+//
+//            int No = 1;
+//            while (T > 0) {
+//                int N = cin.nextInt();
+//                int[] num = new int[N];
+//                for (int i = 0; i < N; i++) {
+//                    num[i] = cin.nextInt();
+//                }
+//                int maxSum = -10000, thisSum = 0;
+//                int start = 0, end = 0;
+//                for (int i = 0; i < N; i++) {
+//                    thisSum += num[i];
+//                    if (thisSum > maxSum) {
+//                        maxSum = thisSum;
+//                        end = i;
+//                    }
+//                    if (thisSum < 0) {
+//                        thisSum = 0;
+//                    }
+//                }
+//                int sum = 0;
+//                for (int i = end; i > -1; i--) {
+//                    sum += num[i];
+//                    if (sum == maxSum) {
+//                        start = i;
+//                    }
+//                }
+//
+//                System.out.println("Case " + No + ":");
+//                System.out.println(maxSum + " " + (start + 1) + " " + (end + 1));
+//                No++;
+//                if (T != 1)
+//                    System.out.println();
+//                T--;
+//
+//            }
+//        }
+//    }
+
+    private static void question1005() {
         Scanner cin = new Scanner(System.in);
         while (cin.hasNext()) {
-            int T = cin.nextInt();
-
-            int No = 1;
-            while (T > 0) {
-                int N = cin.nextInt();
-                int[] num = new int[N];
-                for (int i = 0; i < N; i++) {
-                    num[i] = cin.nextInt();
-                }
-                int maxSum = -10000, thisSum = 0;
-                int start = 0, end = 0;
-                for (int i = 0; i < N; i++) {
-                    thisSum += num[i];
-                    if (thisSum > maxSum) {
-                        maxSum = thisSum;
-                        end = i;
-                    }
-                    if (thisSum < 0) {
-                        thisSum = 0;
-                    }
-                }
-                int sum = 0;
-                for (int i = end; i > -1; i--) {
-                    sum += num[i];
-                    if (sum == maxSum) {
-                        start = i;
-                    }
-                }
-
-                System.out.println("Case " + No + ":");
-                System.out.println(maxSum + " " + (start + 1) + " " + (end + 1));
-                No++;
-                if (T != 1)
-                    System.out.println();
-                T--;
-
+            int n1 = cin.nextInt();
+            int n2 = cin.nextInt();
+            int num = cin.nextInt();
+            if (n1 == 0 && n2 == 0 && num == 0) {
+                return;
             }
+            int[] result = new int[50];
+            result[0] = 1;
+            result[1] = 1;
+            for (int i = 2; i < 50; i++) {
+                result[i] = (n1 * result[i - 1] + n2 * result[i - 2]) % 7;
+            }
+            num =  (num - 1) % 49;
+            System.out.println(result[num]);
         }
     }
 }
